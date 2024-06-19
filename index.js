7c9cebdaf6adbb4db5805e720ed576c8cb10c456
Raul Gamero <raul.a.gamero@gmail.com>
2024-06-19 00:22:30 -0600

    1

--- index.js
+++ index.js
@@ -1138,6 +1138,63 @@ funcionPrincipal(nombre => {
     alert('me llamo ' + nombre);
 });
 
+//FUNCIONES FLECHA
+
+//  Si lleva un parámetro no hace falta poner los paréntesis, si lleva dos ya sí.
+
+function sumame(num1, num2, sumaYmuestra, sumaPorDos){
+    var suma = num1 + num2;
+    sumaYmuestra(suma);
+    sumaPorDos(suma);
+    return suma;
+}
+
+sumame(5, 7, dato => {
+    console.log('La suma es ', dato);
+},
+    dato => {
+        console.log('La suma de dos es ', (dato * 2));
+    });
+
+//EVENTOS
+
+/* Los eventos son la manera que tenemos en
+JavaScript de controlar las acciones de los
+visitantes y definir un comportamiento de la página
+cuando se produzcan. Cuando un usuario visita una
+página web e interactúa con ella se producen los
+eventos y con JavaScript podemos definir qué
+queremos que ocurra cuando se produzcan los
+eventos. */
+
+/*Evento: Es algo que ocurre. Generalmente los
+eventos ocurren cuando el usuario interactúa con
+el documento, pero podrían producirse por
+situaciones ajenas al usuario, como una imagen
+que no se puede cargar porque esté indisponible. */ 
+
+/* Tipo de evento: Es el tipo del suceso que ha
+ocurrido, por ejemplo, un clic sobre un botón o el
+envío de un formulario. Cada tipo de elemento de
+la página ofrece diversos tipos de eventos de
+JavaScript */
+
+/* Manejador de evento: es el comportamiento que
+nosotros podemos asignar como respuesta a un
+evento. Se especifica mediante una función
+JavaScript, que se asocia a un tipo de evento en
+concreto. Una vez asociado el manejador a un
+tipo de evento sobre un elemento de la página,
+da vez que ocurre ese tipo de evento sobre ese
+elemento en concreto, se ejecutará el manejador
+de evento asociado */ 
+
+//En JavaScript podemos definir eventos de dos maneras distintas.
+
+//Manejadores de evento especificados en el codigo HTML  (Revisar HTML)
+
+
+
 
 
 
